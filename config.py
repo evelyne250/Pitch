b97091a9416ec1ea05060566b28b1913ab7297b6
pascaline <passyish5@gmail.com>
2019-09-19 15:00:08 +0200

    initialized the virtual environment

--- config.py
+++ config.py
@@ -2,47 +2,48 @@ import os
 
 class Config:
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:ineza@localhost/watchlist_test'
-
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-
-    #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'Watchlist'
-    SENDER_EMAIL = 'uevelyne44@gmail.com'
-
-# simple mde  configurations
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = 'secretkey'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-    @staticmethod
-    def init_app(app):
-        pass
 
 
 
 class ProdConfig(Config):
-   
-    pass
+    '''
+    Pruduction configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:ineza@localhost/watchlist_test'
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    pass
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:ineza@localhost/watchlist'
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://wecode:ineza@localhost/pitch'
     DEBUG = True
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig,
-'test':TestConfig
 
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
